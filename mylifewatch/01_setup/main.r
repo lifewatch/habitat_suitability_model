96a3fe4bd36eec4cd7f18e70310540aed4903a4f
skoulouzis <koulouzis@gmail.com>
2025-06-04 13:41:09 +0200

    added params

--- mylifewatch/01_setup/main.r
+++ mylifewatch/01_setup/main.r
@@ -12,9 +12,20 @@
 
 ##################################################################################
 ##################################################################################
+path = list(
+  input = "/mnt/inputs/document.txt",
+  output = "/mnt/outputs/result.json",
+  code = "./code"
+)
+
 # FUNCTIONS ---------------------------------------------------------------
 lapply(list.files("functions", full.names = TRUE),source)
+sapply(list.files(path$code, full.names = T), source)
 # INPUT -------------------------------------------------------------------
+
+args = args_parse(commandArgs(trailingOnly = TRUE))
+
+
 
 # WORKFLOW ----------------------------------------------------------------
 # Define different folders
@@ -88,19 +99,22 @@ lapply(package_list, require, character.only = TRUE)
 library(imis)
 
 #Define user input choices
-study_area <- load_ospar(c("II","III"), filepath = file.path(spatdir,"ospar_regions_2017_01_002.shp"))
+study_area <- load_ospar(c("II","III"), filepath = file.path(spatdir,args$study_area_file))
 bbox <- sf::st_bbox(study_area)
-date_start <- as.POSIXct("1999-01-01")
-date_end <- as.POSIXct("2019-12-31")
+date_start <- as.POSIXct(args$start_date)
+date_end <- as.POSIXct(args$end_date)
 temporal_extent <- lubridate::interval(date_start,date_end)
-possible_aphiaids <- c(137117, 137084, 137094, 137111, 137101, 137087)
+# possible_aphiaids <- c(137117, 137084, 137094, 137111, 137101, 137087)
+possible_aphiaids = args$possible_aphiaids
+# Convert possible_aphiaids to numeric if they are not already
+possible_aphiaids <- as.numeric(unlist(strsplit(possible_aphiaids, ",")))
 aphiaid <- possible_aphiaids[1]
 
 #Choosing the bounding box to assess the monthly trend
-min_lon <- 1.8
-max_lon <- 2.5
-min_lat <- 51
-max_lat <- 52
+min_lon <- args$min_lon
+max_lon <- args$max_lon
+min_lat <- args$min_lat
+max_lat <- args$max_lat
 
 # OUTPUT ------------------------------------------------------------------
 
