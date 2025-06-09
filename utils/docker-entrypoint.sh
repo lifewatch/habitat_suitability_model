43c6da233e19eb4f2114c417c9a539fed8391292
skoulouzis <koulouzis@gmail.com>
2025-06-09 17:39:35 +0200

    add entrypoint t base image

--- utils/docker-entrypoint.sh
+++ utils/docker-entrypoint.sh
@@ -8,6 +8,9 @@ if [ ! -f /mnt/inputs/input.zip ]; then
     throwError "No inputs file found."
 fi
 
+# Unzip input files
+unzip -q /mnt/inputs/input.zip -d /mnt/inputs/
+
 Rscript --vanilla main.r
 
 # Zip file in the output folder
