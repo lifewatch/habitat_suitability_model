f01d6a562d4656d5ca42e717be740f0e88b0321a
skoulouzis <koulouzis@gmail.com>
2025-06-10 20:07:20 +0200

    05

--- utils/entrypoint.sh
+++ utils/entrypoint.sh
@@ -1,25 +1,18 @@
 #! /bin/bash
 
-# Unzip input files if input.zip exists
-if [ -f /mnt/inputs/input.zip ]; then
-  cd /mnt/inputs
-  unzip -o input.zip
-fi
+
+for f in /mnt/inputs/*.zip; do
+  [ -e "$f" ] && echo "Unzipping $f" && unzip -o "$f" -d /mnt/inputs
+done
 
 cd /wrp
 
 # Call main.r script with input arguments
 Rscript --vanilla main.r "$@"
 
-# Zip file in the output folder if it exists and if not empty
-if [ -d /mnt/outputs ]; then
-  cd /mnt/outputs
-  if [ "$(ls -A .)" ]; then
-    zip -r output.zip .
-    echo "Output zipped to output.zip"
-  else
-    echo "No files to zip in /mnt/outputs"
-  fi
-else
-  echo "/mnt/outputs directory does not exist."
-fi
+cd /mnt/outputs
+for f in *; do
+  [ -f "$f" ] && echo "Zipping $f" && zip "${f%.*}.zip" "$f"
+done
+
+cd /wrp
