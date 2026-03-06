e02dc2394fa5884491e7dd51e12112134523bf84
johannow <johannes.nowe@vliz.be>
2026-03-06 14:12:33 +0100

    remove hardcoded eurobis url

--- functions/connect_eurobis.R
+++ functions/connect_eurobis.R
@@ -5,7 +5,20 @@ connect_eurobis <- function(){data_lake <- arrow::S3FileSystem$create(
   endpoint_override = "s3.waw3-1.cloudferro.com"
 )
 
-s3_path = "emodnet/emodnet_biology/12639/eurobis_gslayer_obisenv_19022025.parquet"
+#Get out the s3 path from the STAC catalogue instead of hardcoding
+stac_endpoint_url <- 'https://catalog.dive.edito.eu/'
+stac_obj <- rstac::stac(stac_endpoint_url)
+
+emodnet_occurrence <- stac_obj%>%
+  collections("emodnet-occurrence_data")%>%
+  items()%>%
+  get_request()
+
+#gives us the url of the dataset, we need the .parquet file
+urls <- rstac::assets_url(emodnet_occurrence)
+emodnet_url <- urls[grepl("^https://s3\\.waw3-1\\.cloudferro\\.com/.+\\.parquet$", urls)][1]
+s3_path <- stringr::str_split_i(emodnet_url, "s3.waw3-1.cloudferro.com/",2)
+
 eurobis <- arrow::open_dataset(
   s3_path,
   filesystem = data_lake,
